c3cfb06dcd8393f5b195cd981eb8b43d2396d479
Mukta Behera <mbehera@qumu.com>
2020-11-10 09:57:11 -0800

    explicit wait

---
+++ ClearGoogleSearch.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+
+def clear(text,element):
+   for char in text:
+       element.send_keys(Keys.BACK_SPACE)
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+searchtext = "helloworld"
+driver.get("https://www.google.com/")
+s = driver.find_element_by_name("qr")
+s.send_keys(searchtext)
+# s.clear()
+
+# for char in 'selenium':
+#     s.send_keys(Keys.BACK_SPACE)
+
+
+# def cleartext(text):
+#     for char in text:
+#         s.send_keys(Keys.BACK_SPACE)
+#
+# cleartext(searchtext)
+
+clear(searchtext,s)
